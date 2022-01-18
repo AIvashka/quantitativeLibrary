e4db9be29602252e5a6f79ee26dffd068d2dc912
aroud <aroud.by@gmail.com>
2022-01-19 01:46:10 +0300

    Distributions refactoring

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <random>
 #include <iostream>
 #include <vector>
-#include "headers/Distributions.h"
+#include "distributions.h"
 
 int main()
 {
-    std::vector<int> poissonDistribution = Distributions::poisson<100>(5);
+    std::vector<int> poissonDistribution = distributions::GetPoissonSample(100, 5);
     for (auto &value : poissonDistribution) {
         std::cout << value << " ";
     }
